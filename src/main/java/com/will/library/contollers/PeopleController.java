f73699e54f9f468b9eafaceb2fc1d6b326a7e90c
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-02-24 21:33:58 +0500

    create PeopleController

---
+++ src/main/java/com/will/library/contollers/PeopleController.java
@@ -0,0 +1,61 @@
+package com.will.library.contollers;
+
+import com.will.library.dao.PersonDAO;
+import com.will.library.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String findAllPeople(Model model) {
+        model.addAttribute("people", personDAO.findAll());
+        return "people/index";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String save(@ModelAttribute Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.findById(id));
+        return "people/edit";
+    }
+
+    @GetMapping("/{id}")
+    public String finById(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.findById(id));
+        return "people/each";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute("person")Person person) {
+        personDAO.update(person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+}
