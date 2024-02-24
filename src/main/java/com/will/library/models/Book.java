9fd03fb759f3a780b050e8f51c87b32000e8fc26
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-02-24 21:33:25 +0500

    create models

---
+++ src/main/java/com/will/library/models/Book.java
@@ -0,0 +1,56 @@
+package com.will.library.models;
+
+public class Book {
+    private final int id;
+    private String title;
+    private String author;
+    private int year;
+    private Person owner;
+
+    public Book(String title, String author, int year) {
+        this(0, title, author, year);
+    }
+
+    public Book(int id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+}
---
+++ src/main/java/com/will/library/models/Person.java
@@ -0,0 +1,60 @@
+package com.will.library.models;
+
+import java.util.List;
+
+public class Person {
+    private int id;
+    private String fullName;
+    private int yearOfBirth;
+    private List<Book> books;
+
+    public Person() {
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this(0, fullName, yearOfBirth);
+    }
+
+    public Person(int id, String fullName, int yearOfBirth) {
+        this(id, fullName, yearOfBirth, null);
+    }
+
+    public Person(int id, String fullName, int yearOfBirth, List<Book> books) {
+        this.id = id;
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+        this.books = books;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
